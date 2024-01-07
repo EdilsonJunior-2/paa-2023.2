26ae307ca17b6398932357f3d21cef05bc8531c4
Edilson Leite <juniorse13@gmail.com>
2024-01-07 11:48:06 -0300

    Add files via upload

--- mergesort/mergesort.py
+++ mergesort/mergesort.py
@@ -15,13 +15,31 @@ def conquer(out, entry, start_position, middle, final_position):
     start_aux = start_position
     ahead_middle = middle + 1
     start_aux_2 = start_position
-    print(start_position, middle, final_position, out, entry)
-    while(start_aux <= middle & ahead_middle <= final_position):
-        if(entry[start_aux][position] <= entry[ahead_middle][position]):
-            print(start_aux, start_aux_2)
-            out[start_aux_2 + 1] = entry[start_aux + 1]
+    print(start_position == middle)
+    while start_aux <= middle and ahead_middle <= final_position:
+        if entry[start_aux][position] <= entry[ahead_middle][position]:
+            out[start_aux_2] = entry[start_aux]
+            start_aux = start_aux + 1
         else:
-            out[start_aux_2 + 1] = entry[ahead_middle + 1]
+            out[start_aux_2] = entry[ahead_middle]
+            ahead_middle = ahead_middle + 1
+        start_aux_2 = start_aux_2 + 1
+
+    # Copy the remaining elements from the first half
+    print(out)
+    while start_aux <= middle:
+        out[start_aux_2] = entry[start_aux]
+        print("func 1", start_aux_2, out[start_aux_2][position])
+        start_aux = start_aux + 1
+        start_aux_2 = start_aux_2 + 1
+
+    # Copy the remaining elements from the second half
+    while ahead_middle <= final_position:
+        out[start_aux_2] = entry[ahead_middle]
+        print(start_aux_2, out[start_aux_2][position])
+        ahead_middle = ahead_middle + 1
+        start_aux_2 = start_aux_2 + 1
+
 
 def divide(out, entry, start_position, final_position):
     if(start_position < final_position):
@@ -31,13 +49,8 @@ def divide(out, entry, start_position, final_position):
         conquer(out, entry, math.trunc(start_position), math.trunc(middle), math.trunc(final_position))
 
 def main(args):
-    #Ilustrando uso de argumentos de programa
-    print(args)
-    print("#ARGS = %i"%len((args)))
-    print("PROGRAMA = %s"%(args[0]))
-    #Abrindo Arquivos
-    golden_input = open("porto.input",'r')
     golden_output = open("porto.output",'w')
+    golden_input = open("porto.input",'r')
     main_list_count = int(golden_input.readline())
     main_list = []
     for i in range(main_list_count):
@@ -56,9 +69,7 @@ def main(args):
             main_list_count_aux = main_list_count_aux + 1
         arrived_list.append(arrived_item)
         error_list.append([])
-
     divide(error_list, arrived_list, 0, arrived_list_count - 1)
-    print(error_list)
     golden_input.close()
     golden_output.close()
 

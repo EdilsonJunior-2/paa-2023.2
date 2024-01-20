a42df451186a2ca00487b744cf9af545852274ef
Edilson Leite <juniorse13@gmail.com>
2024-01-20 12:33:33 -0300

    Add files via upload

--- mergesort/edilsonleite_202100104001_porto.py
+++ mergesort/edilsonleite_202100104001_porto.py
@@ -6,13 +6,13 @@ cnpj = 1
 weight = 2
 position = 3
 
-def conquer(origin, entry, start_position, middle, final_position, archiveName):
+def conquer(entry, start_position, middle, final_position):
 	start_aux = start_position
 	ahead_middle = middle + 1
 	start_aux_2 = 0
 	temp = [None] * (final_position - start_position + 1)
 	if start_aux + 1 == ahead_middle:
-		if entry[start_aux][position] <= entry[ahead_middle][position]:
+		if entry[start_aux][0] <= entry[ahead_middle][0]:
 			temp[start_aux_2] = entry[start_aux]
 			temp[start_aux_2 + 1] = entry[ahead_middle]
 		else:
@@ -22,7 +22,7 @@ def conquer(origin, entry, start_position, middle, final_position, archiveName):
 		ahead_middle = ahead_middle + 1
 	else:
 		while start_aux <= middle and ahead_middle <= final_position:
-			if entry[start_aux][position] <= entry[ahead_middle][position]:
+			if entry[start_aux][0] <= entry[ahead_middle][0]:
 				temp[start_aux_2] = entry[start_aux]
 				start_aux = start_aux + 1
 			else:
@@ -41,62 +41,64 @@ def conquer(origin, entry, start_position, middle, final_position, archiveName):
 		ahead_middle = ahead_middle + 1
 		start_aux_2 = start_aux_2 + 1
 	
-	if(final_position - start_position  !=  len(entry) - 1):
-		for i in range(final_position - start_position + 1):
-			entry[start_position + i] = temp[i]
-	else:
-		golden_output = open(archiveName,'w')
-		for i in range(final_position + 1):
-			org = origin[temp[i][position]]
-			if temp[i][cnpj] != org[cnpj]:
-				golden_output.write(temp[i][code] + ": " + org[cnpj] + "<->" + temp[i][cnpj] + "\n")
-		for i in range(final_position + 1):
-			org = origin[temp[i][position]]
-			if temp[i][weight] >= org[weight]:
-				difference =  int(temp[i][weight]) - int(org[weight])
-				difference_percentage = (difference/int(temp[i][weight]))*100
-				
-				if difference_percentage > 10:
-					golden_output.write(f'{temp[i][code]}: {difference}kg ({round(difference_percentage)}%)\n')
-					golden_output.flush()
-			else:
-				difference = int(org[weight]) - int(temp[i][weight])
-				difference_percentage = (difference/int(org[weight]))*100
-				
-				if difference_percentage > 10:
-					golden_output.write(f'{temp[i][code]}: {difference}kg ({round(difference_percentage)}%)\n')
-					golden_output.flush()
-		
-		golden_output.close()
+	for i in range(final_position - start_position + 1):
+		entry[start_position + i] = temp[i]
 
-def divide(origin, entry, start_position, final_position, archiveName):
+def divide(entry, start_position, final_position):
 	if(start_position < final_position):
 		middle = start_position + (final_position - start_position)/2
-		divide(origin,entry, math.trunc(start_position), math.trunc(middle), "")
-		divide(origin,entry, math.trunc(middle + 1), math.trunc(final_position), "")
-		conquer(origin, entry, math.trunc(start_position), math.trunc(middle), math.trunc(final_position), archiveName)
+		divide(entry, math.trunc(start_position), math.trunc(middle))
+		divide(entry, math.trunc(middle + 1), math.trunc(final_position))
+		conquer(entry, math.trunc(start_position), math.trunc(middle), math.trunc(final_position))
 
+def returnErrorString(el):
+	return el[1]
 def main(args):
 	golden_input = open(args[1],'r')
 	main_list_count = int(golden_input.readline())
 	main_list = []
-	for i in range(main_list_count):
-		main_list.append(golden_input.readline().split())
+	for _ in range(main_list_count):
+		main_item = golden_input.readline().split()
+		main_item[weight] = int(main_item[weight])
+		main_list.append(main_item)
 
 	arrived_list_count = int(golden_input.readline())
 	arrived_list = []
-	for i in range(arrived_list_count):
+	cnpj_error_list = []
+	cnpj_error_list_count = 0
+	weight_error_list = []
+	weight_error_list_count = 0
+	
+	for _ in range(arrived_list_count):
 		arrived_item = golden_input.readline().split()
+		arrived_item[weight] = int(arrived_item[weight])
 		flag = 0
 		main_list_count_aux = 0
 		while flag == 0:
 			if(arrived_item[0] == main_list[main_list_count_aux][code]):
 				arrived_item.append(main_list_count_aux)
+				if arrived_item[cnpj] != main_list[main_list_count_aux][cnpj]:
+					cnpj_error_list.append([main_list_count_aux, f'{arrived_item[code]}: {main_list[main_list_count_aux][cnpj]}<->{arrived_item[cnpj]}'])
+					cnpj_error_list_count += 1
+				if arrived_item[weight] != main_list[main_list_count_aux][weight]:
+					difference = abs(arrived_item[weight] - main_list[main_list_count_aux][weight])
+					difference_percentage = (difference/arrived_item[weight])*100
+					weight_error_list.append([main_list_count_aux, f'{arrived_item[code]}: {difference}kg ({round(difference_percentage)}%)'])
+					weight_error_list_count += 1
 				flag = 1
 			main_list_count_aux = main_list_count_aux + 1
 		arrived_list.append(arrived_item)
-	divide(main_list, arrived_list, 0, arrived_list_count - 1, args[2])
+	divide(cnpj_error_list, 0, cnpj_error_list_count - 1)
+	divide(weight_error_list, 0, weight_error_list_count - 1)
 	golden_input.close()
+	golden_output = open(args[2], "w")
+
+	for i in range(cnpj_error_list_count):
+		golden_output.write(f'{cnpj_error_list[i][1]}\n')
+	for i in range(weight_error_list_count):
+		golden_output.write(f'{weight_error_list[i][1]}\n')
+
+	golden_output.close()	
 
 if __name__ == '__main__':
 	main(sys.argv)
